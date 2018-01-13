065f6f09208a1de80851736e10f093893b899cd2
Daniel Coady <daniel.coady@outlook.com.au>
2018-01-13 13:59:33 +1100

    Inital commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+const bot = new Discord.Client();
+
+// Object containing commands that can be called by users
+var commands = {
+    "ping": {
+        process: function(bot, message, suffix)
+        {
+            message.channel.send("Pong!");
+        }
+    }
+}
+
+// Check the message is indeed a command
+function CheckCommand(_msg) {
+    if (_msg.author.id != bot.user.id && (_msg.content.startsWith(config.prefix))) {
+            var cmdTxt = _msg.content.split(" ")[0].substring(config.prefix.length);
+            var suffix = _msg.content.substring(cmdTxt.length + config.prefix.length + 1);
+            var cmd = commands[cmdTxt];
+
+            if (cmd) {
+                if (_msg.author == bot.user) return;
+            }
+            cmd.process(bot, _msg, suffix);
+        }
+}
+
+bot.on("ready", () => console.log("Bot logged in"));
+bot.on("message", message => CheckCommand(message));
+
+bot.login(config.token);
