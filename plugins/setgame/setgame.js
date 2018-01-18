f3c114b547ffed96e3470e324c62d826cd0af9af
Daniel Coady <daniel.coady@outlook.com.au>
2018-01-18 17:34:57 +1100

    Added input validaiton to setgame

--- plugins/setgame/setgame.js
+++ plugins/setgame/setgame.js
@@ -5,7 +5,11 @@ exports.commands = ["setgame"];
 exports.init = function(client, config, _) {}
 exports["setgame"] = {
     process: function(bot, message, suffix) {
+        if (suffix.length > 128) {
+            message.channel.send("Sorry, that game title is just too damn long!");
+        } else {
             bot.user.setPresence({ game: { name: suffix } });
             message.channel.send("Set game to " + suffix);
+        }
     }
 }
